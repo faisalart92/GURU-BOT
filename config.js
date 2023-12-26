ded08534872c8fe3322732bfd5a9a4cef668f67f
faisalart92 <154861324+faisalart92@users.noreply.github.com>
2023-12-26 10:25:15 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,11 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
-  [''] 
-] //Number of owners
-
+  ['923227244254', 'faisal', true],
+  ['923446302482', 'fabot', true]
 //global.pairingNumber = "" //put your bot number here
 
 global.mods = ['917605902011','32460248586'] 
